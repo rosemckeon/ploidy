be7f0791ea0d2750279e33ce16558bdea2387873
Rose McKeon <rose@rosemckeon.co.uk>
2019-05-20 09:07:13 +0000

    made dispersal resemble clonal growth over generations. fixes #4.

--- R/init.R
+++ R/init.R
@@ -9,19 +9,29 @@
 #' @return population data for each generation
 disturploidy <- function(
   pop_size = 100,
-  grid_size = 100
+  grid_size = 100,
+  generations = 1
 ){
+  out <- list()
   # populate landscape
-  pop_0 <- populate_landscape(pop_size, grid_size)
+  out$pop_0 <- populate_landscape(pop_size, grid_size)
   # advance time
-  pop_1 <- lapply(pop_0, find_coordinates, move)
-  # return data
-  return(
-    list(
-      pop_0 = pop_0,
-      pop_1 = pop_1
+  for(i in 1:generations){
+    # take the generation before i
+    current_gen <- out[[paste0("pop_", i-1)]]
+    # and apply clonal growth (doubles and disperses population)
+    new_gen <- lapply(
+      current_gen, find_coordinates, move, 1
     )
-  )
+    new_gen <- c(
+      current_gen, new_gen
+    )
+    # label new generation and store
+    element <- paste0("pop_", i)
+    out[[element]] <- new_gen
+  }
+  # return data
+  return(out)
 }
 
 # run the model
