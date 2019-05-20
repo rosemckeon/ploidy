e8102d9ea5e580115698c40f5551402e1ed68e92
Rose McKeon <rose@rosemckeon.co.uk>
2019-05-20 07:49:27 +0000

    Included basic dispersal in the main model function. #4

--- R/init.R
+++ R/init.R
@@ -4,17 +4,25 @@
 #' @name disturploidy
 #' @details Runs the model to determine an answer to our question.
 #' @author Rose McKeon
-#' @param grid_size size of the landscape grid
 #' @param pop_size size of the starting population
+#' @param grid_size size of the landscape grid
 #' @return population data for each generation
 disturploidy <- function(
   pop_size = 100,
   grid_size = 100
 ){
   # populate landscape
-  pop <- populate_landscape(pop_size, grid_size)
+  pop_0 <- populate_landscape(pop_size, grid_size)
+  # advance time
+  pop_1 <- lapply(population, find_coordinates, move)
   # return data
-  return(pop)
+  return(
+    list(
+      pop_0 = pop_0,
+      pop_1 = pop_1
+    )
+  )
 }
+
 # run the model
 disturploidy()
