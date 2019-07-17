b491a8281c647d6be7ded7d8a9f777048a611d38
Rose McKeon <rose@rosemckeon.co.uk>
2019-07-17 15:02:57 +0000

    fixes #55 by capping rate returned to max_rate parameter.

--- R/traits.R
+++ R/traits.R
@@ -2,21 +2,32 @@
 #' @details Uses all alleles at loci to determine growth rate with minimum value of 1.
 #' @author Rose McKeon
 #' @param genome A dataframe contining the genome of an individual
-#' @param loci the loci to use for this trait.
+#' @param loci a numeric vector of integers over 0 (eg 1:5) which represent the loci to use for this trait (default = 1:genome_size).
 #' @param ploidy_benefit A number between 0 and 1 that represents the proportion by which being polyploid improves growth rate.
+#' @param max_rate A number representing the maximum rate which can be output no matter the genes (default = 2, so individuals can never more than double in size in a generation).
 #' @return number greater than or equal to 1 used to multiply size in grow("individuals").
-get_growth_rate <- function(genome, loci = 1:2, ploidy_benefit = 1){
+get_growth_rate <- function(genome, ploidy_benefit = 1, loci = NULL, max_rate = 2){
   # make sure we have the right parameters
   stopifnot(
     is.data.frame(genome),
     "allele" %in% colnames(genome),
     "locus" %in% colnames(genome),
     "value" %in% colnames(genome),
-    nrow(genome) < 0,
-    is.numeric(loci),
+    nrow(genome) > 0,
     is.numeric(ploidy_benefit),
     between(ploidy_benefit, 0, 1)
   )
+  # set loci defaults
+  genome_size <- nlevels(genome$locus)
+  if(is.null(loci)){
+    loci <- 1:genome_size
+  } else {
+    stopifnot(
+      is.numeric(loci),
+      max(loci) <= genome_size,
+      min(loci) > 0
+    )
+  }
   # get all the loci values that contribute to growth rate
   growth_rate_loci <- genome %>%
     filter(locus %in% loci) %>%
@@ -25,42 +36,35 @@ get_growth_rate <- function(genome, loci = 1:2, ploidy_benefit = 1){
   # see how many alelles we have
   n <- length(growth_rate_loci)
 
-  # determine trait value for ploidy_benefit = 0
+  # Determine trait value for ploidy_benefit = 0
+  # --------------------------------------------
   # (the average value across the genome determines growth rate)
   # seeing as max_trait_val = min_trait_val * n
   # min_trait_val is also equal to 1/n of the max_trait_val
   min_trait_val <- mean(growth_rate_loci)
 
-  # get the number of n used to find max_trait_val - min_trait_val
+  # Get the number of n used to find max_trait_val - min_trait_val
+  # --------------------------------------------------------------
   # so, if n = 4, then we need 3/4 of max_trait_val, ie: 3
   # Or, if n = 8, then we need 7/8 of of max_trait_val, ie: 7
   numerator <- ((n - 1) / n) * n
 
-  # determine trait value for all other ploidy_benefit scenarios
+  # Determine trait value for all ploidy_benefit scenarios
+  # -------------------------------------------------------------
   # the product of the maths contained in the brackets will range
-  # from 0 to the full difference between max and min trait vals
-  # depending on ploidy_benefit.
+  # from 0 to numerator/n of the max_trait_val depending on ploidy_benefit.
+  # adding to 1/n of the max_trait_val (ie: min_trait_val) produces a
+  # trait value which is properly adjusted by the ploidy_benefit constant.
   trait_val <- min_trait_val + (min_trait_val * numerator * ploidy_benefit)
 
-  # convert trait value to rate
-  # BD: I don't think that there is anything wrong with the below to increase
-  #     growth rate, but it might still get out of hand if there is no maximum
-  #     on `value` in the genome. If the value maximum is 100, then this should
-  #     cap things nicely.
-  growth_rate <- exp(.001 * trait_val)
-  # BD: I think it would be good to somehow avoid the hard code of `0.001`, and
-  #     instead do some quick calculation to allow for a maximum growth rate of
-  #     a diploid population (i.e., if all alleles at all loci had a value of
-  #     100). If, e.g., we want the maximum possible diploid growth rate to be
-  #     2, then we can do the following (growth_rates = 2000, x is unknown):
-  #
-  #     2     = e^(2000 * x)
-  #     ln(2) = 2000 * x
-  #     x     = ln(2) / 2000 = 0.0003465736
-  #
-  #     So the code could calculate the above if a parameter `max_growth_rate`
-  #     was set to 2, solving for x to flexibly replace the 0.001 above.
-  #     Let me know if you need some help coding this.
-
+  # Return trait value as rate
+  # ---------------------------
+  # work out max trait value possible
+  # (if allele values are constrained between 1 and 100)
+  ploidy_lvl <- nlevels(genome$allele)
+  max_trait_val <- ploidy_lvl * genome_size * 100
+  # do conversion (with rate constrained to max_rate)
+  growth_rate <- exp((log(max_rate) / max_trait_val) * trait_val)
+  # return rate
   return(growth_rate)
 }
