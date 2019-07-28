76ab8e802b6b38e7f7505bf810680aec7e3a3643
Rose McKeon <rose@rosemckeon.co.uk>
2019-07-28 21:10:17 +0000

    added a script for running many simulations #61

---
+++ analysis/simulations.R
@@ -0,0 +1,40 @@
+rm(list=ls())
+#library(DisturPloidy)
+library(tidyverse)
+library(tictoc)
+source("ploidy/R/disturploidy.R")
+source("ploidy/R/functions.R")
+source("ploidy/R/traits.R")
+
+# Saving lots of output...
+name <- "null"
+generations <- 200
+simulations <- 4
+runs <- 1
+
+for(run in runs){
+  # save a new object for every simulation
+  this_run <- paste0(name, "-", run)
+  disturploidy(
+    pop_size = 500,
+    grid_size = 40,
+    N_ovules = 25,
+    pollen_range = 40,
+    seed_survival_prob = .5325,
+    germination_prob = .5,
+    ploidy_growth_benefit = 0,
+    inbreeding_sensitivity = 0,
+    fertilisation_prob = .5,
+    uneven_matching_prob = .5,
+    selfing_diploid_prob = 0,
+    selfing_polyploid_prob = 0,
+    triploid_mum_prob = .5,
+    disturbance_freq = 1000,
+    generations = generations,
+    simulations = simulations,
+    filepath = "ploidy/analysis/data/",
+    filename = this_run,
+    logfilepath = "ploidy/analysis/data/logs/",
+    logfilename = this_run
+  )
+}
