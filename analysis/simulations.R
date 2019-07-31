b7fd3e80b06be1be96b346e6fbe45a68ea84eb03
Rose McKeon <rose@rosemckeon.co.uk>
2019-07-31 00:45:54 +0000

    new sims for #61

--- analysis/simulations.R
+++ analysis/simulations.R
@@ -1,8 +1,8 @@
 rm(list=ls())
-#library(DisturPloidy)
 library(tidyverse)
 library(devtools)
 library(tictoc)
+# library(disturploidy)
 source("../R/disturploidy.R")
 source("../R/functions.R")
 source("../R/traits.R")
@@ -11,20 +11,22 @@ source("../R/traits.R")
 name <- "null"
 generations <- 200
 simulations <- 4
-runs <- 2:4
+runs <- 2
 
 for(run in runs){
   # save a new object for every simulation
   this_run <- paste0(name, "-", run)
   disturploidy(
-    pop_size = 500,
+    pop_size = 400,
     grid_size = 40,
+    juvenile_selection_constant = .1,
+    adult_survival_prob = .7,
+    inbreeding_sensitivity = 0,
+    germination_prob = .5,
+    seed_survival_prob = 0,
+    ploidy_growth_benefit = 0,
     N_ovules = 25,
     pollen_range = 40,
-    seed_survival_prob = .534,
-    germination_prob = .5,
-    ploidy_growth_benefit = 0,
-    inbreeding_sensitivity = 0,
     fertilisation_prob = .5,
     uneven_matching_prob = .5,
     selfing_diploid_prob = 0,
