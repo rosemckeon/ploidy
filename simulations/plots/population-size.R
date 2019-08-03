c34d9b7f55a94b7ba46c47bde2e46fc4f6e319e7
Rose <rosemckeon@gmail.com>
2019-08-03 22:20:48 +0100

    updated plots for #61 with estiamted seed pop sizes and seeds removed from ploidy plots

--- simulations/plots/population-size.R
+++ simulations/plots/population-size.R
@@ -12,7 +12,7 @@ library(ggplot2)
 
 name <- "null"
 exportpath = "simulations/plots/population-size/"
-runs <- 1:4
+runs <- 1:2
 
 for(run in runs){
   # get the data
@@ -20,43 +20,43 @@ for(run in runs){
     paste0("simulations/data/", name, "-", run, ".rds")
   )
 
-  # # plot poulation by life stage
-  # source("simulations/plots/correct-seed-counts.R")
-  #
-  # filename <- paste0(name, "-", run, "-life_stage.pdf")
-  #
-  # qplot(
-  #   as.numeric(gen),
-  #   n,
-  #   data = counts,
-  #   geom = "line",
-  #   colour = as.factor(life_stage)
-  # ) + theme_classic() + theme(
-  #   legend.position = "top",
-  #   strip.background = element_rect(
-  #     colour = "#d1d1d1",
-  #     fill = "#d1d1d1"
-  #   )
-  # ) + geom_hline(
-  #   yintercept = 1600,
-  #   color = "#d1d1d1",
-  #   size = 1
-  # ) + scale_colour_discrete(
-  #   name = "Life stage",
-  #   labels = c("Seeds", "Juveniles", "Adults"),
-  #   h = c(20, 180),
-  #   l = 50
-  # ) + labs(
-  #   tag = toupper(paste(name, run)),
-  #   caption = "Population size over time (grey line represents K)."
-  # ) + xlab("Generation") + ylab("Population size")
-  #
-  # ggsave(
-  #   filename, path = exportpath, device = "pdf",
-  #   width = 11.69, height = 8.27, units = "in",
-  #   dpi = "retina",
-  #   title = paste(toupper(paste(name, run)), "population size by life stage.")
-  # )
+  # plot poulation by life stage
+  source("simulations/plots/correct-seed-counts.R")
+
+  filename <- paste0(name, "-", run, "-life_stage.pdf")
+
+  qplot(
+    as.numeric(gen),
+    n,
+    data = counts,
+    geom = "line",
+    colour = as.factor(life_stage)
+  ) + theme_classic() + theme(
+    legend.position = "top",
+    strip.background = element_rect(
+      colour = "#d1d1d1",
+      fill = "#d1d1d1"
+    )
+  ) + geom_hline(
+    yintercept = 1600,
+    color = "#d1d1d1",
+    size = 1
+  ) + scale_colour_discrete(
+    name = "Life stage",
+    labels = c("Seeds", "Juveniles", "Adults"),
+    h = c(20, 180),
+    l = 50
+  ) + labs(
+    tag = toupper(paste(name, run)),
+    caption = "Population size over time (grey line represents K)."
+  ) + xlab("Generation") + ylab("Population size")
+
+  ggsave(
+    filename, path = exportpath, device = "pdf",
+    width = 11.69, height = 8.27, units = "in",
+    dpi = "retina",
+    title = paste(toupper(paste(name, run)), "population size by life stage.")
+  )
 
   # plot population by ploidy level
   ploidy_pop_sizes <- sim$data %>%
