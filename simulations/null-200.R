db5dfc6fdae0c8b8e2bd0edba35f2dba9feea3e4
Rose McKeon <rose@rosemckeon.co.uk>
2019-08-15 11:29:15 +0000

    updated null-200 sim based on new default params

--- simulations/null-200.R
+++ simulations/null-200.R
@@ -2,16 +2,12 @@ rm(list=ls())
 library(tidyverse)
 library(devtools)
 library(tictoc)
-# library(disturploidy)
-# or source the scripts from this dir as wd
-source("../R/disturploidy.R")
-source("../R/functions.R")
-source("../R/traits.R")
+library(disturploidy)
 
 name <- "null-200"
 generations <- 200
 simulations <- 1
-runs <- 2
+runs <- 1:10
 
 # saves a new data file for every run which contains
 # the number of simulations specified. A separate
@@ -21,24 +17,8 @@ for(run in runs){
   disturploidy(
     pop_size = 1000,
     grid_size = 40,
-    juvenile_selection_constant = .1,
-    adult_survival_prob = .5,
-    inbreeding_cost = 0,
-    germination_prob = .4,
-    seed_survival_prob = 0,
-    ploidy_prob = 0,
-    adult_size = 1.3,
-    ploidy_growth_benefit = 0,
-    max_growth_rate = 4,
-    N_ovules = 25,
-    pollen_range = 40,
-    fertilisation_prob = .75,
-    uneven_matching_prob = .75,
-    selfing_diploid_prob = .75,
-    selfing_polyploid_prob = .75,
-    triploid_mum_prob = .75,
+    pollen_range = 39,
     seed_dispersal_range = 39,
-    disturbance_freq = 0,
     generations = generations,
     simulations = simulations,
     filename = this_run,
